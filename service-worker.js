9fd539caec4896d152d7cc2cd659055efa0e7d0f
karolduarte1512-rgb <karolduarte1512@gmail.com>
2026-08-22 01:25:01 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = 'portal-vision-pwa-v1';
+const CACHE_NAME = 'portal-vision-pwa-v2';
 
-const ARQUIVOS_BASE = [
+const SHELL = [
   './',
   './index.html',
+  './config.js',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png'
@@ -10,7 +11,7 @@ const ARQUIVOS_BASE = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS_BASE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL))
   );
   self.skipWaiting();
 });
@@ -29,8 +30,19 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-  );
+  const url = new URL(event.request.url);
+
+  // Só fazemos cache da casca da PWA.
+  // O Portal do Apps Script continua online e dinâmico.
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
 });
