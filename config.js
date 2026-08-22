3e8cc3396096ed4740fa7b8c7ead5bc20200dbc0
karolduarte1512-rgb <karolduarte1512@gmail.com>
2026-08-22 01:23:16 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Cole abaixo a URL /exec atual da implantação do Portal Vision.
+const PORTAL_VISION_URL =
+  'https://script.google.com/macros/s/AKfycbyf-T7fN-P7qGANhMESMRPiPP4xz-mHmvNnmFM8p2IHpx3NBU6quzvY2vleIMqYrEPQpQ/exec';
